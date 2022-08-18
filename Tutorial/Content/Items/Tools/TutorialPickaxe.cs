801664c78ce71aaf43bd92a40c46cef9cc9754cf
MateuszDKowalewski <aspoka1@gmail.com>
2022-08-18 23:26:15 +0200

    Add tutorial pickaxe

---
+++ Tutorial/Content/Items/Tools/TutorialPickaxe.cs
@@ -0,0 +1,47 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+
+namespace BigCityBilgoraj.Tutorial.Content.Items.Tools
+{
+    internal class TutorialPickaxe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Tutoriasl Pickaxe");
+            Tooltip.SetDefault("This is tutorial pickaxe");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+
+            Item.useTime = 15;
+            Item.useAnimation = 13;
+            Item.autoReuse = true;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 12;
+            Item.knockBack = 3f;
+
+
+            Item.value = Item.buyPrice(silver: 20);
+            Item.rare = ItemRarityID.Blue;
+
+            Item.pick = 60;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddRecipeGroup(RecipeGroupID.Wood, 8)
+                .AddIngredient(ModContent.ItemType<TutorialItem>(), 4)
+                .Register();
+        }
+    }
+}
