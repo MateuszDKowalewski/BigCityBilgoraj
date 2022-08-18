33aff4fb921fa43f85fe9ed9913a70f2a3e3ffe8
MateuszDKowalewski <aspoka1@gmail.com>
2022-08-18 23:13:34 +0200

    Add configuration

--- BigCityBilgoraj.cs
+++ BigCityBilgoraj.cs
@@ -1,8 +1,9 @@
-using Terraria.ModLoader;
+﻿using Terraria.ModLoader;
 
 namespace BigCityBilgoraj
 {
 	public class BigCityBilgoraj : Mod
 	{
-	}
+        public const string ASSET_PATH = "BigCityBiłgoraj/Assets/";
+    }
 }
