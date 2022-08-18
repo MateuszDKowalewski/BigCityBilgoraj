fca524c6fc3890764e11e7187b8ea6f4de48305d
MateuszDKowalewski <aspoka1@gmail.com>
2022-08-18 23:45:54 +0200

    Add tutorial ore

---
+++ Tutorial/Content/Items/Placebles/TutorialOre.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+
+namespace BigCityBilgoraj.Tutorial.Content.Items.Placebles
+{
+    internal class TutorialOre : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Tutorial Ore");
+            Tooltip.SetDefault("This is tutorial ore");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+            ItemID.Sets.SortingPriorityMaterials[Type] = 58;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = 999;
+            Item.consumable = true;
+            Item.value = Item.buyPrice(copper: 50);
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+        }
+
+    }
+}
