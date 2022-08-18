0c988fdf4d087a4d9076fc4072c8cdd994892fcb
MateuszDKowalewski <aspoka1@gmail.com>
2022-08-18 23:17:58 +0200

    Add tutorial iitem

---
+++ Tutorial/Content/Items/TutorialItem.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+namespace BigCityBilgoraj.Tutorial.Content.Items
+{
+    internal class TutorialItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Tutoriasl Item");
+            Tooltip.SetDefault("This is tutorial item");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+
+            Item.value = Item.buyPrice(copper: 5);
+            Item.maxStack = 999;
+        }
+    }
+}
